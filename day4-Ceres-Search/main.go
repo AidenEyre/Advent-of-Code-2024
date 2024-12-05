8859659e3b60a06b10e23aa53190ba93420f671c
AidenEyre <EyreAiden@gmail.com>
2024-12-04 22:48:37 -0700

    chore: day 4 part 2

--- day4-Ceres-Search/main.go
+++ day4-Ceres-Search/main.go
@@ -22,6 +22,7 @@ type WordsearchSolver5000 struct {
 	widthAndLength int
 	WordLength     int
 	Word           string
+	xy             [2]int
 }
 
 func main() {
@@ -32,11 +33,14 @@ func main() {
 	solver := &WordsearchSolver5000{
 		wordMatrix:     wordsearch,
 		widthAndLength: len(wordsearch),
-		Word:           "XMAS",
+		Word:           "MAS",
+		// Word:           "XMAS",
 	}
-	wordsFound := solver.searchForWords(solver.Word[0], "regular")
+	// wordsFoundStraight := solver.searchForWords(solver.Word[0], "regular")
+	wordsFoundCross := solver.searchForWords(solver.Word[1], "cross")
 
-	fmt.Printf("WordsearchSolver5000 found '%d' instances of '%s'", wordsFound, solver.Word)
+	// fmt.Printf("WordsearchSolver5000 found '%d' instances of '%s' STRAIGHT", wordsFoundStraight, solver.Word)
+	fmt.Printf("WordsearchSolver5000 found '%d' instances of '%s' CROSS", wordsFoundCross, solver.Word)
 }
 
 func loadDataIntoSlice(filename string) ([][]byte, error) {
@@ -60,26 +64,41 @@ func loadDataIntoSlice(filename string) ([][]byte, error) {
 	return dataSlice, nil
 }
 
-func (ws *WordsearchSolver5000) inBoundsFromPoint(x, y int, direction string) bool {
+func (ws *WordsearchSolver5000) inBoundsFromPoint(direction string) bool {
 	max := ws.widthAndLength - 1
 	min := 0
+	steps := len(ws.Word) - 1
 
-	dir, exists := directions[direction]
-	if !exists {
-		fmt.Printf("Invalid direction: %s\n", direction)
-		return false
+	if direction == "all" {
+		return ws.inBoundsAll(min, max)
 	}
 
-	steps := len(ws.Word) - 1
-	endX := x + dir[0]*steps
-	endY := y + dir[1]*steps
-
+	dir := directions[direction]
+	endX := ws.xy[0] + dir[0]*steps
+	endY := ws.xy[1] + dir[1]*steps
 	if endX < min || endY < min || endX > max || endY > max {
 		return false
 	}
 	return true
 }
 
+func (ws *WordsearchSolver5000) inBoundsAll(min, max int) bool {
+	for direction := range directions {
+		dir := directions[direction]
+		tmpX := ws.xy[0] + dir[0]
+		tmpY := ws.xy[1] + dir[1]
+		if tmpX < min || tmpY < min || tmpX > max || tmpY > max {
+			return false
+		}
+	}
+	return true
+}
+
+// searchForWords ... Solves crossword puzzle
+//
+//   - searchType=regular - 8 directions: horizontal, vertical, and diagonal.
+//   - searchType=cross - requires the word to appear twice in a cross pattern with
+//     startChar being the middle.
 func (ws *WordsearchSolver5000) searchForWords(startChar byte, searchType string) int {
 	totalWords := 0
 
@@ -88,12 +107,13 @@ func (ws *WordsearchSolver5000) searchForWords(startChar byte, searchType string
 			if ws.wordMatrix[x][y] != startChar {
 				continue
 			}
+			ws.xy = [2]int{x, y}
 
 			if searchType == "regular" {
-				totalWords += ws.searchStraightFromPoint(x, y)
+				totalWords += ws.searchStraightFromPoint()
 			}
 			if searchType == "cross" {
-				totalWords += ws.searchCrossFromPoint(x, y)
+				totalWords += ws.searchCrossFromPoint()
 			}
 		}
 	}
@@ -101,37 +121,76 @@ func (ws *WordsearchSolver5000) searchForWords(startChar byte, searchType string
 	return totalWords
 }
 
-func (ws *WordsearchSolver5000) searchStraightFromPoint(x, y int) int {
+func (ws *WordsearchSolver5000) searchStraightFromPoint() int {
 	count := 0
 	for direction := range directions {
-		if ws.inBoundsFromPoint(x, y, direction) && ws.checkForMatchStraight(x, y, direction) {
+		if ws.inBoundsFromPoint(direction) && ws.checkForMatchStraight(direction) {
 			count++
 		}
 	}
 	return count
 }
 
-func (ws *WordsearchSolver5000) checkForMatchStraight(x, y int, direction string) bool {
+func (ws *WordsearchSolver5000) checkForMatchStraight(direction string) bool {
 	steps := len(ws.Word)
-	dir, exists := directions[direction]
-	if !exists {
-		fmt.Printf("Invalid direction: %s\n", direction)
-		return false
-	}
+	dir := directions[direction]
 
-	// fmt.Printf("\nDIRECTION: %s\n(%d,%d) - %s\n", direction, x, y, string(ws.wordMatrix[x][y]))
+	tmpX := ws.xy[0]
+	tmpY := ws.xy[1]
 	for i := 1; i < steps; i++ {
-		x = x + dir[0]
-		y = y + dir[1]
-		// fmt.Printf("(%d,%d) - %s\n", x, y, string(ws.wordMatrix[x][y]))
-		if ws.wordMatrix[x][y] != ws.Word[i] {
+		tmpX = tmpX + dir[0]
+		tmpY = tmpY + dir[1]
+		if ws.wordMatrix[tmpX][tmpY] != ws.Word[i] {
 			return false
 		}
 	}
-	// fmt.Println("HERE")
 	return true
 }
 
-func (ws *WordsearchSolver5000) searchCrossFromPoint(x, y int) int {
-	return 0
+func (ws *WordsearchSolver5000) searchCrossFromPoint() int {
+	if !ws.inBoundsFromPoint("all") {
+		return 0
+	}
+
+	firstChar := ws.Word[0]
+	lastChar := ws.Word[len(ws.Word)-1]
+
+	topLeft := ws.getCorner("upLeft")
+	if topLeft != firstChar && topLeft != lastChar {
+		return 0
+	}
+
+	bottomRight := ws.getCorner("downRight")
+	if bottomRight != ws.getOppositeWordChar(topLeft) {
+		return 0
+	}
+
+	topRight := ws.getCorner("upRight")
+	if topRight != firstChar && topRight != lastChar {
+		return 0
+	}
+
+	bottomLeft := ws.getCorner("downLeft")
+	if bottomLeft != ws.getOppositeWordChar(topRight) {
+		return 0
+	}
+
+	return 1
+}
+
+func (ws *WordsearchSolver5000) getCorner(direction string) byte {
+	dir := directions[direction]
+	tmpX := ws.xy[0] + dir[0]
+	tmpY := ws.xy[1] + dir[1]
+	return ws.wordMatrix[tmpX][tmpY]
+}
+
+func (ws *WordsearchSolver5000) getOppositeWordChar(input byte) byte {
+	firstChar := ws.Word[0]
+	lastChar := ws.Word[len(ws.Word)-1]
+
+	if input == firstChar {
+		return lastChar
+	}
+	return firstChar
 }
